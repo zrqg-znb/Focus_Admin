1d7bfe0c039cbb0690782323d0c0f22724a3a2e0
张瑞卿 <1315993841@qq.com>
2025-11-23 01:19:02 +0800

    新增迭代管理基础页面

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,24 @@ const naiveLocale = computed(() => {
 </script>
 
 <template>
-  <n-config-provider
-    class="wh-full" inline-theme-disabled :theme="appStore.colorMode === 'dark' ? darkTheme : null"
-    :locale="naiveLocale.locale" :date-locale="naiveLocale.dateLocale" :theme-overrides="appStore.theme"
-  >
-    <naive-provider>
-      <router-view />
-      <Watermark :show-watermark="appStore.showWatermark" />
-    </naive-provider>
-  </n-config-provider>
+  <div class="bg-background wh-full">
+    <n-config-provider
+      inline-theme-disabled :theme="appStore.colorMode === 'dark' ? darkTheme : null"
+      :locale="naiveLocale.locale" :date-locale="naiveLocale.dateLocale" :theme-overrides="appStore.theme"
+    >
+      <naive-provider>
+        <router-view />
+        <Watermark :show-watermark="appStore.showWatermark" />
+      </naive-provider>
+    </n-config-provider>
+  </div>
 </template>
+
+<style scoped>
+.bg-background{
+  /* 设置全局页面背景图 */
+  background: url('@/assets/backgrounds/back1.jpg') no-repeat center center fixed;
+  background-size: cover;
+  min-height: 100vh;
+}
+</style>
--- src/service/api/product.ts
+++ src/service/api/product.ts
@@ -1,28 +1,28 @@
 import { request } from '@/service/http'
 
 export const productApi = {
-  // 获取产品分类
-  getProductCategoryList(params: object) {
-    return request.Get('/release_gap/categories/', { params })
-  },
-
-  // 创建产品分类
-  createProductCategory(data: object) {
-    return request.Post('/release_gap/categories/', data)
-  },
-  // 更新产品分类
-  updateProductCategory(id: string, data: object) {
-    return request.Put(`/release_gap/categories/${id}/`, data)
-  },
-  // 删除产品分类
-
   // 获取产品列表
+  getProductList(params: any) {
+    return request.Get('/iterations/products/', { params })
+  },
 
   // 创建产品
+  createProduct(data: any) {
+    return request.Post('/iterations/products/', data)
+  },
 
   // 获取产品详情
+  getProduct(id: number) {
+    return request.Get(`/iterations/products/${id}/`)
+  },
 
   // 更新产品
+  updateProduct(id: number, data: any) {
+    return request.Put(`/iterations/products/${id}/`, data)
+  },
 
   // 删除产品
+  deleteProduct(id: number) {
+    return request.Delete(`/iterations/products/${id}/`)
+  },
 }
